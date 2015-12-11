9f2e00e755642a986b344d542fd50f41ccc64980
Kyle Gilliam <kyle.gilliam@agilent.com>
2015-12-10 18:33:20 -0700

    Renamed Chapter3, Added functions to ch5functions.hs

--- Chapter 5/ch5functions.hs
+++ Chapter 5/ch5functions.hs
@@ -12,3 +12,11 @@ isUpperAlpha = (`elem` ['A'..'Z'])
 
 applyTwice :: (a -> a) -> a -> a
 applyTwice f x = f (f x)
+
+zipWith' :: (a -> b -> c) -> [a] -> [b] -> [c]
+zipWith' _ [] _ = []
+zipWith' _ _ [] = []
+zipWith' f (x:xs) (y:ys) = f x y : zipWith' f xs ys
+
+flip' :: (a -> b -> c) -> b -> a -> c
+flip' f y x = f x y
