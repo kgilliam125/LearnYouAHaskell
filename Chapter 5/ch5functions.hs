5938ee12f8b833b181933ae72382a3ec1dac26db
Kyle L. Gilliam <me@kylelgilliam.com>
2015-12-16 22:06:43 -0700

    Added more fold functions from Ch. 5

--- Chapter 5/ch5functions.hs
+++ Chapter 5/ch5functions.hs
@@ -61,3 +61,21 @@ map'' f xs = foldl (\acc x -> acc ++ [f x]) [] xs
 
 elem' :: (Eq a) => a -> [a] -> Bool
 elem' y ys = foldr (\x acc -> if x == y then True else acc) False ys
+
+maximum' :: (Ord a) => [a] -> a
+maximum' = foldl1 max
+
+reverse' :: [a] -> [a]
+reverse' = foldl (\acc x -> x : acc) []
+
+reverse'' :: [a] -> [a]
+reverse'' = foldl ( flip (:)) []
+
+product' :: (Num a) => [a] -> a
+product' = foldl (*) 1
+
+filter' :: (a -> Bool) -> [a] -> [a]
+filter' p = foldr (\x acc -> if p x then x : acc else acc) []
+
+last' :: [a] -> a
+last' = foldl1 (\_ x -> x)
