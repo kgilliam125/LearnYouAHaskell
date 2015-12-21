69a9d253a074a4987d7805d054ce96c76d44b6ef
Kyle Gilliam <kyle.gilliam@agilent.com>
2015-12-21 14:22:11 -0700

    Added Geometry and Geometry.* modules. Added files for phoneBook

--- Chapter 6/findKey.hs
+++ Chapter 6/findKey.hs
@@ -1,3 +1,12 @@
+phoneBook = 
+    [("betty", "555-2938")
+    ,("bonnie", "452-2928")
+    ,("patsy", "493-2928")
+    ,("lucille", "205-2928")
+    ,("wendy", "939-2928")
+    ,("penny", "853-2928")
+    ]
+
 findKey :: (Eq k) => k -> [(k,v)] -> v
 findKey key xs = snd . head . filter (\(k,v) -> key == k) $ xs 
 
@@ -6,3 +15,6 @@ findKey' _ [] = Nothing
 findKey' key ((k,v):xs)
     | key == k = Just v
     | otherwise = findKey' key xs
+
+findKey'' :: (Eq k) => k -> [(k,v)] -> Maybe v
+findKey'' key xs = foldr (\(k, v) acc -> if key == k then Just v else acc) Nothing xs
