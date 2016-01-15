34e5cd25cda0a9973825c1509ce46ad72edb313c
Kyle L Gilliam <me@kylelgilliam.com>
2016-01-14 22:17:49 -0700

    Added 3 more functions from Chapter 14

---
+++ Chapter 14/diffList.hs
@@ -0,0 +1,7 @@
+newtype DiffList a = DiffList { getDiffList :: [a] -> [a] }
+
+toDiffList :: [a] -> DiffList a
+toDiffList xs = DiffList (xs++)
+
+fromDiffList :: DiffList a -> [a]
+fromDiffList (DiffList f) = f []    
