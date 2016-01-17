f0b283ebe826691e706299f35a25a656615e25bb
Kyle L Gilliam <me@kylelgilliam.com>
2016-01-17 12:33:39 -0700

    Copied diffLists into gcd to test easily. Added versions of gcdReverse which do/don't use DiffList for comparison

--- Chapter 14/diffList.hs
+++ Chapter 14/diffList.hs
@@ -1,7 +1,12 @@
 newtype DiffList a = DiffList { getDiffList :: [a] -> [a] }
 
+instance Monoid (DiffList a) where
+    mempty = DiffList (\xs -> [] ++ xs)
+    (DiffList f) `mappend` (DiffList g) = DiffList (\xs -> f (g xs))
+
 toDiffList :: [a] -> DiffList a
 toDiffList xs = DiffList (xs++)
 
 fromDiffList :: DiffList a -> [a]
 fromDiffList (DiffList f) = f []    
+
--- Chapter 14/gcd.hs
+++ Chapter 14/gcd.hs
@@ -1,5 +1,19 @@
 import Control.Monad.Writer
 
+newtype DiffList a = DiffList { getDiffList :: [a] -> [a] }
+
+instance Monoid (DiffList a) where
+    mempty = DiffList (\xs -> [] ++ xs)
+    (DiffList f) `mappend` (DiffList g) = DiffList (\xs -> f (g xs))
+
+toDiffList :: [a] -> DiffList a
+toDiffList xs = DiffList (xs++)
+
+fromDiffList :: DiffList a -> [a]
+fromDiffList (DiffList f) = f []    
+
+
+
 gcd' :: Int -> Int -> Writer [String] Int
 gcd' a b
     | b == 0    = do
@@ -18,3 +32,27 @@ gcdReverse a b
         result <- gcdReverse b (a `mod` b)
         tell [show a ++ " mod " ++ show b ++ " = " ++ show (a `mod` b)]
         return result
+
+gcdReverse' :: Int -> Int -> Writer (DiffList String) Int
+gcdReverse' a b 
+    | b == 0    = do
+        tell (toDiffList ["Finished with " ++ show a])
+        return a
+    | otherwise = do
+        result <- gcdReverse' b (a `mod` b)
+        tell (toDiffList [show a ++ " mod " ++ show b ++ " = " ++ show (a `mod` b)])
+        return result
+
+finalCountDown :: Int -> Writer (DiffList String) ()
+finalCountDown 0 = do
+    tell (toDiffList ["0"])
+finalCountDown x = do
+    finalCountDown (x - 1)
+    tell (toDiffList [show x])
+
+finalCountDown' :: Int -> Writer [String] ()
+finalCountDown' 0 = do
+    tell ["0"]
+finalCountDown' x = do
+    finalCountDown' (x - 1)
+    tell [show x]
